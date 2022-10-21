47682cbe2e307dbf5a47e460d59272930b11681e
GKakaz <dim_kak@msn.com>
2022-10-21 15:42:12 +0300

    updates

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ContactInfos from '../contactinfos/ContactInfos'
 import Footer from '../footer/Footer'
 import './_portfolio.scss'
 import Intro from '../intro/Intro'
-
+import { ProfilieCard } from '../card/ProfilieCard'
+import Skills from '../skills/Skills'
+import './_portfolio.scss'
 
 const Portfolio = () => {
 
-  
+  const [content, setContent] = useState(false);
+
+  const contentStatus = () => {
+    setContent(prev => !prev)
+  }
 
   return (
-    <>    
+    <>
+     <h1 className='m-1 p-1 text-center'><button className='skills'  onClick={contentStatus}>Welcome to George Kakaziani's portfolio.</button></h1>
      <Intro />
-     <Footer />
+     <div className={`${content ? "see" : "hide"}`} id='cont'>
+     <ProfilieCard />
+     <ContactInfos />
+     <Skills />
+     </div>
+    <Footer />
     </>
   )
 }
